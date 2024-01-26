e5ff2b7d43801568f8d1fdbb48ff3f52518f004b
unknown <valeriovenceslai@gmail.com>
2024-01-26 01:06:20 +0100

    Theme selection

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ RED = '\033[91m'
 GREEN = '\033[92m'
 ENDC = '\033[0m'
 
+quiz_sections = {
+    'teoria_dello_scafo' : (0,125),
+    'motori' : (126,229),
+    'sicurezza della navigazione' : (230,444),
+    'manovra e condotta' : (445,599),
+    'colreg e segnalamento marittimo' : (600,846),
+    'meteorologia' : (847,966),
+    'navigazione cartografica ed elettronica' : (967,1288),
+    'normativa diportistica': (1288,1472)
+}
+
 def process_question(row):
     cleaned_row = [col.strip() for col in row if col.strip()]  # Remove leading/trailing spaces from each non-empty column
     
@@ -34,8 +45,29 @@ def evaluate_answer(user_answer, correct_answer_idx, correct_answer):
 def main():
     quiz_file_path = "quiz_entro.csv"  # Replace with the actual path to your CSV file
     
-    
+    # Display keys and wait for user input
+    print("Choose a quiz section:")
+    for idx, section in enumerate(quiz_sections.keys()):
+        print(f"{idx}: {section}")
 
+    # Get user input
+    user_input = input("Enter the number corresponding to the quiz section: ")
+
+    # Check if the input is a valid number
+    if user_input.isdigit():
+        section_idx = int(user_input)
+
+        # Check if the index is within the valid range
+        if 0 <= section_idx < len(quiz_sections):
+            selected_section = list(quiz_sections.keys())[section_idx]
+            print(f"You selected: {selected_section}")
+
+            target_tuple = quiz_sections[selected_section]
+        else:
+            print("Invalid input. Please enter a number within the specified range.")
+    else:
+        print("Invalid input. Please enter a valid number.")
+    
     with open(quiz_file_path, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
         header = next(reader)  # Skip the header
@@ -46,9 +78,15 @@ def main():
         
         rows=list(reader)
         
-        random.shuffle(rows)
+        new_rows = list()
 
-        for row in rows:
+        for idx, el in enumerate(rows):
+            if idx in range(target_tuple[0], target_tuple[1]):
+                new_rows.append(el)
+
+        random.shuffle(new_rows)
+
+        for row in new_rows:
             
             question_idx, question, answer1, is_true1, answer2, is_true2, answer3, is_true3 = process_question(row)
             display_question(question_idx, question, answer1, answer2, answer3)
